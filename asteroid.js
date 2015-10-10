0e6f601d3cf5084d12b0ce63934a3aef2c06f8d8
Braelan <bbraelan@gmail.com>
2015-10-10 05:59:15 -0700

    created front page css

---
+++ asteroid.js
@@ -0,0 +1,22 @@
+// (function() {
+//   if (typeof Asteroid === "undefined") {
+//     window.Asteroid = {};
+//   }
+//
+//   // will hopefully toggle class
+//   Asteroid.hide = function () {
+//     $(.info).toggleClass("on")
+//   }
+//
+// })();
+$(document).ready(function(){
+
+  $('button').click(function() {
+    $(".info").toggleClass("off");
+
+    new Asteroids.GameView(
+      canvasEl.width,
+      canvasEl.height
+    ).start(canvasEl);
+  })
+})
